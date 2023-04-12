bd879bf499b1ad8dbb73d889d1c2beea1fde3381
omar ouhagua <omarouhga12@gmail.com>
2023-04-12 04:18:32 +0000

    some_changes in welcome page

--- models/reservation.py
+++ models/reservation.py
@@ -10,7 +10,7 @@ class CUReservation(models.Model):
     date_reservation = fields.Date(string="Date de réservation",default=datetime.today(),required=True)
     date_consommation=fields.Date(string="Date de consommation",required=True)
     type = fields.Selection([('DJ','déjeuner'),('D','Diner')],required=True,string="Type")
-
+    
     def validate_mail(self):
         if self.email:
             match = re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', self.email)
