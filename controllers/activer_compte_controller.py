9d3eaafdbe6145da4b58d8e329325f2ca353eb9f
omar ouhagua <omarouhga12@gmail.com>
2023-05-13 02:05:14 +0100

    some ameliorations

--- controllers/activer_compte_controller.py
+++ controllers/activer_compte_controller.py
@@ -13,47 +13,47 @@ class AccountConfirmationController(http.Controller):
             user = request.env['cu.resident'].sudo().search([('email', '=', email)])
             if not user:
                 raise UserError('User with email %s not found' % email) 
-            token = str(uuid.uuid4())
-            user.sudo().write({'password': token,
-                               'confirmation_token':token})
-            confirmation_url = request.httprequest.host_url + 'auth/login'
-            body_html = """
-                <html>
-                    
-                    <body>
-                        <div >
-                            <h3>Bonjour <b>{user_name}</b>,</h3>
-                            <p>Nous sommes ravis que vous ayez créé un compte chez nous. Pour assurer la sécurité de votre compte, nous vous demandons de changer votre mot de passe après la première connexion.</p>
-                            <p>Votre mot de passe temporaire est:</p>
-                            <h4>{token}</h4>
-                            <p>Veuillez utiliser le lien ci-dessous pour accéder à votre compte et changer votre mot de passe:</p>
-                            <p><a href="{confirmation_url}">{confirmation_url}</a></p>
-                            <p>Si vous avez des questions ou des préoccupations, n'hésitez pas à nous contacter. Nous sommes toujours là pour vous aider.</p>
-                            <p>Cordialement,<br>L'équipe de {company_name}</p>
-                        </div>
-                    </body>
-                </html>
-                """.format(
-                    user_name=user.name,
-                    token=token,
-                    confirmation_url=confirmation_url,
-                    company_name='TDI SOCITY',
-                )
-
-
-            
-            
-            mail_values = {
-                'subject': 'Activer votre compte Réstauration CU',
-                'body_html': body_html,
-                'email_from': 'omarouhga12@gmail.com',
-                'email_to': email,
-            }
-            account_actived=user.sudo().account_actived
+            else:
+                account_actived=user.sudo().account_actived
            
-            if account_actived==False:
-                request.env['mail.mail'].sudo().create(mail_values).send()
-                user.sudo().write({'account_actived': True})
+                if account_actived==False:
+                    
+                   
+                    token = str(uuid.uuid4())
+                    user.sudo().write({'password': token,
+                                    'confirmation_token':token})
+                    confirmation_url = request.httprequest.host_url + 'auth/login'
+                    body_html = """
+                        <html>
+                            
+                            <body>
+                                <div >
+                                    <h3>Bonjour <b>{user_name}</b>,</h3>
+                                    <p>Nous sommes ravis que vous ayez créé un compte chez nous. Pour assurer la sécurité de votre compte, nous vous demandons de changer votre mot de passe après la première connexion.</p>
+                                    <p>Votre mot de passe temporaire est:</p>
+                                    <h4>{token}</h4>
+                                    <p>Veuillez utiliser le lien ci-dessous pour accéder à votre compte et changer votre mot de passe:</p>
+                                    <p><a href="{confirmation_url}">{confirmation_url}</a></p>
+                                    <p>Si vous avez des questions ou des préoccupations, n'hésitez pas à nous contacter. Nous sommes toujours là pour vous aider.</p>
+                                    <p>Cordialement,<br>L'équipe de {company_name}</p>
+                                </div>
+                            </body>
+                        </html>
+                        """.format(
+                            user_name=user.name,
+                            token=token,
+                            confirmation_url=confirmation_url,
+                            company_name='TDI SOCITY',
+                        )
+                    mail_values = {
+                        'subject': 'Activer votre compte Réstauration CU',
+                        'body_html': body_html,
+                        'email_from': 'omarouhga12@gmail.com',
+                        'email_to': email,
+                    }
+                    request.env['mail.mail'].sudo().create(mail_values).send()
+                    user.sudo().write({'account_actived': True})
+                
             
         return request.render('gr_cu.activer_compte',{'message':account_actived})
 
